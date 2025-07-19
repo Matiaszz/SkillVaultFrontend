a5e01b1fd439b1ad92e8438d4b8f633f8d144b35
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2025-07-19 11:17:11 -0300

    chore: defining user profile interface

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,6 @@
 import { ParamValue } from 'next/dist/server/request/params';
 import api from './api';
-import { UserResponseDTO } from '@/app/responses';
+import { UserResponseDTO, UserProfileDTO } from '@/app/responses';
 
 
 export const UserService = {
@@ -8,6 +8,11 @@ export const UserService = {
     getUserById: async (id: ParamValue) => {
         const normalizedId = Array.isArray(id) ? id[0] : id;
         const res = await api.get<UserResponseDTO>(`/user/${normalizedId}`);
+        return res.data;
+    },
+    getUserProfileInfos: async (id: ParamValue) => {
+        const normalizedId = Array.isArray(id) ? id[0] : id;
+        const res = await api.get<UserProfileDTO>(`/user/profile/${normalizedId}`);
         return res.data;
     }
 };
